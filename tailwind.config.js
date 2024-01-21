b0509ce2b93bac5c89f9bf1c05a0f81ee59dc20e
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-01-21 17:40:01 +0600

    tailwind configure setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fd5442",
+        secondary: "#ebedef",
+      },
+      fontFamily: {
+        inter : [inter, 'sans-serif']
+        lato : [Lato, 'sans-serif']
+        SourceSerif4 : ['Source Serif 4', 'serif']
+      }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
